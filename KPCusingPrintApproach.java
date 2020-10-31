5d684789f361da0908c1c4cb90da7f7c571d7fcc
arisharizvi <73754985+arisharizvi@users.noreply.github.com>
2020-10-31 23:06:42 +0530

    KPC

---
+++ KPCusingPrintApproach.java
@@ -0,0 +1,13 @@
+
+
+import java.util.Scanner;
+
+public class KPCusingPrintApproach {
+	static Scanner scn = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
